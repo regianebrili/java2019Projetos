2b99beb36b48dab384f2d1607e65c9c4af60aa2f
Regiane <regiane.bmf@gmail.com>
2019-12-06 11:29:40 -0300

    SellerForm

---
+++ proj_javafx/src/gui/SellerFormController.java
@@ -0,0 +1,140 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import gui.listeners.DataChangeListener;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+	
+	private Seller entity;
+	
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	// declarar os atributos das duas caixas de texto da tela (Id e Name)
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	// label de mensagem de erro
+	@FXML
+	private Label labelErrorName;
+	
+	// botões
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	// instância do departament
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	// metodos para tratar as ações dos botões
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
+		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	// coloca nas caixas de texto da tela os dados quem vieram no entity
+	public void updateFormDate() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));  // como o Id é número precisa ser convertido para string
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+		
+	}
+}
--- proj_javafx/src/gui/SellerListController.java
+++ proj_javafx/src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -105,28 +110,28 @@ public class SellerListController implements Initializable, DataChangeListener {
 	
 	// carrega a janela para cadastrar um novo departamento
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));  // carrega a view informada no absoluteName
-//			Pane pane = loader.load();  // carrega a view
-//			
-//			// inserir o obj no controlador
-//			SellerFormController controller = loader.getController();  // pega o controlador da tela informada no parâmetro
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this);  // chama o listener para monitorar essa classe
-//			controller.updateFormDate(); // carrega os dados no obj para informar na tela
-//			
-//			Stage dialogStage = new Stage();  // precisa instanciar um novo stage para que a tela fique modal (na frente da tela anterior)
-//			dialogStage.setTitle("Enter Seller data");  // título da janela
-//			dialogStage.setScene(new Scene(pane));  // cria uma nova scena, já que é outro stage
-//			dialogStage.setResizable(false);  // pode ou não ser redimencionada
-//			dialogStage.initOwner(parentStage);	// quem é o stage pai da janela
-//			dialogStage.initModality(Modality.WINDOW_MODAL);  // é modal -> trava a janela, enquanto não for fechada não acessa a anterior
-//			dialogStage.showAndWait();
-//		}
-//		catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));  // carrega a view informada no absoluteName
+			Pane pane = loader.load();  // carrega a view
+			
+			// inserir o obj no controlador
+			SellerFormController controller = loader.getController();  // pega o controlador da tela informada no parâmetro
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this);  // chama o listener para monitorar essa classe
+			controller.updateFormDate(); // carrega os dados no obj para informar na tela
+			
+			Stage dialogStage = new Stage();  // precisa instanciar um novo stage para que a tela fique modal (na frente da tela anterior)
+			dialogStage.setTitle("Enter Seller data");  // título da janela
+			dialogStage.setScene(new Scene(pane));  // cria uma nova scena, já que é outro stage
+			dialogStage.setResizable(false);  // pode ou não ser redimencionada
+			dialogStage.initOwner(parentStage);	// quem é o stage pai da janela
+			dialogStage.initModality(Modality.WINDOW_MODAL);  // é modal -> trava a janela, enquanto não for fechada não acessa a anterior
+			dialogStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
