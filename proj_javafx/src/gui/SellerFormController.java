6bd92f59a972e925a37f3ae0debd5ae393856a33
Regiane <regiane.bmf@gmail.com>
2019-12-06 15:03:55 -0300

    Salvando Seller

--- proj_javafx/src/gui/SellerFormController.java
+++ proj_javafx/src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -136,6 +138,26 @@ public class SellerFormController implements Initializable{
 		}
 		obj.setName(txtName.getText());
 		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			// variável do tipo Instant para pegar o valor que está no DatePicker
+			obj.setBirthDate(Date.from(instant));  // o setBirthDate espera um dado do tipo date, para converter o instant usa o date.from
+		}
+		
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDoublet(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -196,10 +218,19 @@ public class SellerFormController implements Initializable{
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 		
-		if (fields.contains("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+//		if (fields.contains("name")) {
+//			labelErrorName.setText(errors.get("name"));
+//		} else {
+//			labelErrorName.setText("");
+//		}
 		
+// pode fazer o if com else para todos ou da seguinte forma (operador ternário):
+		
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+							//  caso seja verdadeiro ? executa : ou executa esse
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
 	}
 	
 	private void initializeComboBoxDepartment() {
