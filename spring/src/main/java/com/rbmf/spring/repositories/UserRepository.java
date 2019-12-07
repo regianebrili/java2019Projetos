324f41e5125e013994c2b7d4da2a8c6463508ba0
Regiane <regiane.bmf@gmail.com>
2019-12-07 11:21:36 -0300

    Camada de serviço, registro de componentes

--- spring/src/main/java/com/rbmf/spring/repositories/UserRepository.java
+++ spring/src/main/java/com/rbmf/spring/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rbmf.spring.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	// como o userRepository já está herdando do JpaRepository, então não precisa colocar a notação de @Repository
 	
-	
-
 }
---
+++ spring/src/main/java/com/rbmf/spring/services/UserService.java
@@ -0,0 +1,30 @@
+package com.rbmf.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rbmf.spring.entities.User;
+import com.rbmf.spring.repositories.UserRepository;
+
+//  @Component   // registra a classe como um componente do spring e poderá ser injetado automaticamente com o @Autowired
+//  @Repository  // registra um repositório
+@Service         // registra um serviço
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	// como o UserRepository já está herdando do JpaRepository, então não precisa colocar a notação de @Repository lá no UserRepository
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
