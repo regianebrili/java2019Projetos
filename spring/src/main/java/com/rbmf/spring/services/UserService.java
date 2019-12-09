7c5c6ed0038ef9ac2de832ccbef31958619e4903
Regiane <regiane.bmf@gmail.com>
2019-12-09 18:00:24 -0300

    Tratamento de exceção - delete

--- spring/src/main/java/com/rbmf/spring/services/UserService.java
+++ spring/src/main/java/com/rbmf/spring/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rbmf.spring.entities.User;
 import com.rbmf.spring.repositories.UserRepository;
+import com.rbmf.spring.services.exceptions.DatabaseException;
 import com.rbmf.spring.services.exceptions.ResourceNotFoundException;
 
 //  @Component   // registra a classe como um componente do spring e poderá ser injetado automaticamente com o @Autowired
@@ -35,7 +38,13 @@ public class UserService {
 	
 	// apagar o usuário no banco
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	// atualizar o usuário
