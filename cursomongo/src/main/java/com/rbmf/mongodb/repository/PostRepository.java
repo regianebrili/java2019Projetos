d46c9ccc0fbae8e0eb69f5258460841d63f1f057
Regiane <regiane.bmf@gmail.com>
2019-12-13 16:37:28 -0300

    Consulta simples com query methods

--- cursomongo/src/main/java/com/rbmf/mongodb/repository/PostRepository.java
+++ cursomongo/src/main/java/com/rbmf/mongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.rbmf.mongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.rbmf.mongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {		// herdar da MongoRepository<tipo da classe de dominio, tipo do id da classe>
 // no MongoRepository já está embudido todas as operações necessárias.. salvar, apagar, consultar, alterar...
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);		// findByCAMPO_DA_TABELAContaining (o spring data já monta a consulta automaticamente) IgnoreCase para ignorar maículas e minúsculas
 }
