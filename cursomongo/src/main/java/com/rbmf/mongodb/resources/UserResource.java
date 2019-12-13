dddfbd48af67bffbf5aa92081afcd3dd820efcb1
Regiane <regiane.bmf@gmail.com>
2019-12-13 10:47:10 -0300

    Inserção de usuário com POST

--- cursomongo/src/main/java/com/rbmf/mongodb/resources/UserResource.java
+++ cursomongo/src/main/java/com/rbmf/mongodb/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.rbmf.mongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rbmf.mongodb.domain.User;
 import com.rbmf.mongodb.dto.UserDTO;
@@ -38,5 +42,17 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {		//@PathVariable usada para que esse id bata com o id do caminho
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));		//converte o obj para UserDTO
+	}
+	
+	
+	// inserir novo usuário
+	@RequestMapping(method = RequestMethod.POST)  // @PostMapping tb pode ser usado
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = (service.fromDTO(objDto));		// converte o dto para user
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// retorna o caminho com o novo dado
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- cursomongo/src/main/java/com/rbmf/mongodb/services/UserService.java
+++ cursomongo/src/main/java/com/rbmf/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rbmf.mongodb.domain.User;
+import com.rbmf.mongodb.dto.UserDTO;
 import com.rbmf.mongodb.repository.UserRepository;
 import com.rbmf.mongodb.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,15 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	// é melhor colocar no UserService e não no UserDTO, pq dependendo da situação, para instanciar um user, pode ser necessário acessar o banco de dados
+	// e quem tem a dependência para o acesso é o UserService, sendo assim, prevendo um futuro, é melhor colocar aqui.
+	public User fromDTO(UserDTO objDto) {		// vai pegar o DTO e instanciar em um obj (ao contrário da classe DTO)
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
+
