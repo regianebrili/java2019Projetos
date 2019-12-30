a64b67a768667bbb53d3355f8e79a0b303354023
Regiane <regiane.bmf@gmail.com>
2019-12-30 09:36:48 -0300

    Movimentos possíveis de uma peça

--- xadrez/src/chess/pieces/Rook.java
+++ xadrez/src/chess/pieces/Rook.java
@@ -15,4 +15,9 @@ public class Rook extends ChessPiece {	// torre
 		return "R";		// é a letra que vai aparecer no tabuleiro
 	}
 
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
