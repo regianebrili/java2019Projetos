cd5b85d2d4f58c9fcc173509bb81b6a477b18a68
Regiane <regiane.bmf@gmail.com>
2020-01-19 15:46:23 -0300

    Jogada especial Roque (Castling)

--- xadrez/src/chess/pieces/King.java
+++ xadrez/src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 	
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -20,6 +24,12 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();	// se a posição é null ou a cor da peça é adversária
+	}
+	
+	// testar se a torre está apta para o Roque (Castling) - jogada especial
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
@@ -74,6 +84,30 @@ public class King extends ChessPiece {
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// #movimento especial de Roque (specialmove castling)
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// testar se as duas posições estão vagas e se a torre ainda não foi movida (#specialmove castling kingside rook)
+			// mesma linha do rei e a coluna do rei + 3
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+			if (testRookCastling(posT1)) {  // verifica se tem uma torre nessa posição
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1); // pega a casa logo a direita do rei
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2); // pega a 2 casas a direita do rei
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {  // se não tem peça na posição p1 e p2
+					mat[position.getRow()][position.getColumn() + 2] = true;  // informa a posição válida
+				}
+			}
+			// movimento especial de Roque no lado esquerdo, conhecido como Roque grande (#specialmove castling queenside rook)
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if (testRookCastling(posT2)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+			}
 		}
 
 		return mat;
